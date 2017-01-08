8653727a88ef099f4308e13d908779013f49aaf7
admin <gsingh622@yahoo.com>
2017-01-07 21:06:08 -0500

    2

---
+++ Muse/LoginViewController.swift
@@ -0,0 +1,78 @@
+//
+//  LoginViewController.swift
+//  Output
+//
+//  Created by Admin on 11/1/16.
+//  Copyright © 2016 Admin. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var appLogo: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var learnMore: UIButton!
+    var player: AVPlayer?
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        loadPlayer()
+
+    }
+    
+    func loadPlayer(){
+        let path = Bundle.main.path(forResource: "bgVideo", ofType: "mp4")
+        let player = AVPlayer(url: NSURL(fileURLWithPath: path!) as URL)
+        let playerLayer = AVPlayerLayer(player: player)
+        playerLayer.frame = self.view.frame
+        playerLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+        self.view.layer.addSublayer(playerLayer)
+        player.seek(to: kCMTimeZero)
+        player.play()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(LoginViewController.playerItemDidReachEnd(notification:)), name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: player.currentItem)
+        
+        self.view.addSubview(appLogo)
+        self.view.addSubview(descriptionLabel)
+        self.view.addSubview(learnMore)
+        
+    }
+    func playerItemDidReachEnd(notification: NSNotification) {
+
+        if let playerItem: AVPlayerItem = notification.object as? AVPlayerItem {
+            playerItem.seek(to: kCMTimeZero)
+            loadPlayer()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        UIView.animate(withDuration: 2.0, delay: 0.0, options: [], animations: {
+            //self.appLogo.frame = CGRect(origin: CGPoint(x: 38,y:0), size: CGSize(width: 300, height: 150))
+            self.appLogo.frame.origin = CGPoint(x: 40, y: 0)
+            }, completion: nil)
+        UIView.animate(withDuration: 1.5, delay: 2.0, options: [], animations: {
+            self.learnMore.alpha = 1.0
+            }, completion: nil)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
